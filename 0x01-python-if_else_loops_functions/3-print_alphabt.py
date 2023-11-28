8cd7d10cd2826605fced5f341bb75f43a0494580
MairaQuillQuest <ririsoniah@gmail.com>
2023-11-28 10:04:52 -0800

    program that prints the ASCII alphabet

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-#3-print_alphabt.py
-#Maira Wangui
+# 3-print_alphabt.py
+# Maira Wangui
 
 for letter in range(97, 123):
-    if chr(letter) is not 'q' and chr(letter) is not 'e':
+    if chr(letter) != 'q' and chr(letter) != 'e':
         print("{}".format(chr(letter)), end="")
