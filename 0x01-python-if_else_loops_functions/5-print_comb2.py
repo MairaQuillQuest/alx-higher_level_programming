b9423dc1d54179e0e7e2664680e8bb5b302ee2dd
MairaQuillQuest <ririsoniah@gmail.com>
2023-11-28 10:17:59 -0800

    00...99

---
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 5-print_comb2.py
+# Maira Wangui
+
+for number in range(0, 100):
+    if number == 99:
+        print("{}".format(number))
+    else:
+        print("{:02}".format(number), end=", ")
