dc4d9f1f9e2b12f9d06195303a5e6811d30d182e
MairaQuillQuest <ririsoniah@gmail.com>
2023-11-30 10:39:17 -0800

    Everything can be imported

---
+++ 0x02-python-import_modules/5-variable_load.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# 5-variable_load.py
+# Maira Wangui
+
+if __name__ == "__main__":
+    """Print the value of variable a from variable_load_5."""
+    from variable_load_5 import a
+
+    print(a)#!/usr/bin/python3
+# 5-variable_load.py
+# Maira Wangui
+
+if __name__ == "__main__":
+    """Print the value of variable a from variable_load_5."""
+    from variable_load_5 import a
+
+    print(a)
