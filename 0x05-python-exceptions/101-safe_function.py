a366cb86281df4459785e46290f3b310344f7496
MairaQuillQuest <ririsoniah@gmail.com>
2023-12-18 09:23:38 -0800

    Safe function

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -18,7 +18,28 @@ def safe_function(fct, *args):
     """
     try:
         result = fct(*args)
-        return (result)
-    except:
-        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
-        return (None)
+        return result
+    except Exception as e:
+        print("Exception: {}".format(e), file=sys.stderr)
+        return None
+
+
+# Example usage
+def my_div(a, b):
+    return a / b
+
+result = safe_function(my_div, 10, 2)
+print("result of my_div: {}".format(result))
+
+result = safe_function(my_div, 10, 0)
+print("result of my_div: {}".format(result))
+
+def print_list(my_list, length):
+    i = 0
+    while i < length:
+        print(my_list[i])
+        i += 1
+    return length
+
+result = safe_function(print_list, [1, 2, 3, 4], len([1, 2, 3, 4]))
+print("result of print_list: {}".format(result))
