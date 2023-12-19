b80982c181829c5fdd7933bf78f9e1ea9aa3f913
MairaQuillQuest <ririsoniah@gmail.com>
2023-12-19 11:13:59 -0800

    Print Square instance

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -2,23 +2,55 @@
 # 101-safe_function.py
 # Maira Wangui
 
-import sys
-
-
-def safe_function(fct, *args):
-    """Executes a function safely.
-
-    Args:
-        fct: The function to execute.
-        args: Arguments for fct.
-
-    Returns:
-        If an error occurs - None.
-        Otherwise - the result of the call to fct.
+class Square:
+    """ A class that defines a square by its size
     """
-    try:
-        result = fct(*args)
-        return (result)
-    except:
-        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
-        return (None)
+    def __eq__(self, other):
+        return self.__size == other.__size
+
+    def __lt__(self, other):
+        return self.__size < other.__size
+
+    def __le__(self, other):
+        return self.__size <= other.__size
+
+    def __ne__(self, other):
+        return self.__size != other.__size
+
+    def __gt__(self, other):
+        return self.__size > other.__size
+
+    def __ge__(self, other):
+        return self.__size >= other.__size
+
+    def __init__(self, size=0):
+        """ Method to initialize the square object
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """ Method that returns the square are of the object
+        """
+        return (self.__size ** 2)
+
+    @property
+    def size(self):
+        """ Method to returns the size value
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """ Method to set the size value of the square object
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value<F11>
