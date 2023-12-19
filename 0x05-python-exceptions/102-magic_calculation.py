03f5db56fac08228a2fa8d01d1e97b78014ce199
MairaQuillQuest <ririsoniah@gmail.com>
2023-12-19 12:29:03 -0800

    ByteCode -> Python #4

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -4,13 +4,15 @@
 
 def magic_calculation(a, b):
     result = 0
+
     for i in range(1, 3):
         try:
-            if (i > a):
+            if i > a:
                 raise Exception("Too far")
             else:
                 result += (a ** b) / i
-        except:
+        except Exception as e:
             result = b + a
             break
-    return (result)
+
+    return result
