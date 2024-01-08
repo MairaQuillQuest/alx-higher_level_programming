665b239b6cc68f5b0aa41c8947aaca8ba5983361
MairaQuillQuest <ririsoniah@gmail.com>
2024-01-08 12:33:54 -0800

    Edit test cases

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -2,15 +2,5 @@
 # 1-my_list.py
 # Maira Wangui
 class MyList(list):
-    """ Class that inherits the attributes references of class list
-
-    Args:
-        list: class list
-
-    """
-
     def print_sorted(self):
-        """ Method that prints the sorted list """
-        l_sorted = self.copy()
-        l_sorted.sort()
-        print(l_sorted)
+        print(sorted(self))
--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -2,22 +2,10 @@
 # 7-base_geometry.py
 # Maira Wangui
 class BaseGeometry:
-    """ Class that defines the attributes of Geometric Shapes """
-
     def area(self):
-        """ Method that defines the area of a geomtric shape """
-
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """ Method that recieves the value property
-
-        Árgs:
-            name: name of the object
-            value: value of the property
-
-        """
-
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
