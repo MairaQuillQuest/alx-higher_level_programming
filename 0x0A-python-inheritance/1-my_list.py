28c3f0ffba090b835d4ce2e50e926c8811729ed2
MairaQuillQuest <ririsoniah@gmail.com>
2024-01-08 12:49:49 -0800

    Edit test cases

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,6 +1,18 @@
 #!/usr/bin/python3
 # 1-my_list.py
 # Maira Wangui
+"""
+Module 1-my_list
+Contains class MyList
+inherits fr_om list; has public instance method to print sorted
+"""
+
+
 class MyList(list):
+    """inherits fr_om list
+    methods:
+    print_sorted(self)
+    """
     def print_sorted(self):
+        """prints list of ints all sorted in ascending order"""
         print(sorted(self))
