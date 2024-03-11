ebe2a2bbac1950e80d79276cffd0e18103bb6bfe
MairaQuillQuest <ririsoniah@gmail.com>
2024-03-11 13:58:59 -0700

    Adding JavaScript - Warm up Project

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,6 @@
 #!/usr/bin/node
-for (const line of ['C is fun', 'Python is cool', 'Javascript is amazing']) {
-  console.log(line);
+const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let i;
+for (i of arr) {
+  console.log(i);
 }
